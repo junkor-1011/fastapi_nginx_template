cc8a1d1d77d5a1e1dd66e5b948fff695ff6c2a19
junkor-1011 <tomoyukizxy@gmail.com>
2020-08-16 01:04:52 +0900

    trial: add web-page

--- app/app/main.py
+++ app/app/main.py
@@ -2,12 +2,45 @@
 app main
 """
 
-from fastapi import FastAPI
+import pathlib
 
-app = FastAPI()
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import RedirectResponse
+
+from .routers import subpage
+
+# const
+PATH_STATIC = str(pathlib.Path(__file__).resolve().parent / "static")
+
+
+def create_app():
+    """create app"""
+    _app = FastAPI()
+
+    _app.include_router(
+        subpage.router,
+        prefix="/subpage",
+        tags=["subpage"],
+        responses={404: {"description": "not found"}},
+    )
+
+    # static
+    _app.mount(
+        "/static",
+        StaticFiles(directory=PATH_STATIC, html=False),
+        name="static",
+    )
+
+    return _app
+
+
+app = create_app()
 
 
 @app.get('/')
-async def site_root():
-    """root"""
-    return {"message": "Hello, WORLD!"}
+async def redirect_subpage():
+    """redirect webpage"""
+    return RedirectResponse(
+        "/subpage",
+    )
---
+++ app/app/static/subpage/test.js
@@ -0,0 +1,8 @@
+// test subpage
+
+const test_var = "test";
+
+$(document).ready(() => {
+    console.log("document-ready");
+    console.log(test_var);
+});
