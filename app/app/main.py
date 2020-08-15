a7f809be51eb25c496d2e26373b04d694a2c75c4
test-user <test@email.com>
2020-08-15 17:13:19 +0900

    simple setup

--- app/app/main.py
+++ app/app/main.py
@@ -2,55 +2,12 @@
 app main
 """
 
-import pathlib
-
-# from fastapi import Depends, FastAPI, Header, HTTPException
 from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
-from fastapi.responses import RedirectResponse
 
-# from .routers import test_api
-from .routers import subpage
-
-# const
-PATH_STATIC = str(pathlib.Path(__file__).resolve().parent / "static")
-
-
-def create_app():
-    """create app"""
-    _app = FastAPI()
-
-    _app.include_router(
-        subpage.router,
-        prefix="/subpage",
-        tags=["subpage"],
-        responses={404: {"description": "not found"}},
-    )
-
-    # static
-    _app.mount(
-        "/static",
-        StaticFiles(directory=PATH_STATIC, html=False),
-        name="static",
-    )
-
-    return _app
-
-
-app = create_app()
+app = FastAPI()
 
 
 @app.get('/')
 async def site_root():
-    """hello-world(TMP)"""
+    """root"""
     return {"message": "Hello, WORLD!"}
-
-
-@app.get('/redirect_subpage')
-async def redirect_subpage():
-    """redirect test"""
-    # status_codeは何故か効かない
-    return RedirectResponse(
-        "/subpage",
-        # status_code=303,
-    )
--- app/app/static/subpage/test.js
+++
@@ -1,9 +0,0 @@
-// test subpage
-
-let test_var = "test";
-
-$(document).ready( () => {
-    // ***
-    console.log("document-ready");
-    console.log(test_var);
-});
